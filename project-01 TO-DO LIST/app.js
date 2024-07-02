c51bd62f0354268dbd63c61db9a11097c9250c4e
syedaemanmumtaz <syedaeman027@gmail.com>
2024-07-02 08:23:07 -0700

    project-01

---
+++ project-01 TO-DO LIST/app.js
@@ -0,0 +1,30 @@
+function addTask() {
+    const taskInput = document.getElementById('new-task');
+    const taskText = taskInput.value;
+
+    if (taskText === '') {
+        alert('Please enter a task');
+        return;
+    }
+
+    const taskList = document.getElementById('task-list');
+
+    const taskItem = document.createElement('li');
+    taskItem.innerText = taskText;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'Delete';
+    deleteButton.onclick = () => {
+        taskList.removeChild(taskItem);
+    };
+
+    taskItem.appendChild(deleteButton);
+
+    taskItem.onclick = () => {
+        taskItem.classList.toggle('completed');
+    };
+
+    taskList.appendChild(taskItem);
+
+    taskInput.value = '';
+}
